4adf0495bb43ff20aae4e7503a5d5486fb5e1b41
Marcelo Silveira <marcelosj3@hotmail.com>
2021-09-29 17:47:56 -0300

    add style to the info and winner message script
    Added style to all info in the different media queries;
    Added a message when winning;
    Added a blur effect when winning along with a modal to disable clicks;
    Corrected the no message appearing bug on different difficulties;
    Created script to display winner message along with all the text;
    Created script to hide the winner message and restart the game.

--- scripts/script.js
+++ scripts/script.js
@@ -99,21 +99,6 @@ function validate(newDisc, oldDisc, tower) {
     return true
 }
 
-//Fução mensagem de vitória
-
-gameSpace.addEventListener("click", victory)
-const message = document.createElement("div")
-
-function victory() {
-    let pile2Count = pile2.childElementCount
-    let pile3Count = pile3.childElementCount
-    if (pile2Count === diskQuantity || pile3Count === diskQuantity) {
-        message.innerText = "Parabéns! Você ganhou!"
-        gameSpace.appendChild(message)
-        message.appendChild(resetButton)
-    }
-}
-
 // Função Reset
 
 const resetButton = document.getElementById("reset")
@@ -127,8 +112,6 @@ function reset() {
     }
 
     stepCountReset();
-    message.innerText = '';
-    page.appendChild(resetButton)
 }
 
 // SELECTING DIFFICULTY
@@ -139,6 +122,8 @@ difficultySelector.addEventListener('change', changeDifficulty);
 function changeDifficulty() {
     diskQuantity = difficultySelector.value;
     element = null;
+    const stepCountValue = document.querySelector('#stepCountValue');
+    stepCountValue.innerText = 0;
     cleanTowers();
     createDisks(diskQuantity, pile1);
 }
@@ -178,4 +163,103 @@ function stepCount() {
 function stepCountReset() {
     steps = 0;
     stepCountValue.innerText = steps;
+}
+
+function createVictoryMessage(whichDifficulty, stepsMin, playerSteps) {
+    const messageWin = document.createElement('div');
+    const title1 = document.createElement('h3');
+    const title2 = document.createElement('h3');
+    const textWin = document.createElement('p');
+    const winnerButton = document.createElement('button');
+
+    messageWin.id = 'messageWinner';
+    messageWin.classList.add('message-winner')
+    title1.classList.add('winner__title');
+    title2.classList.add('winner__title');
+    winnerButton.classList.add('winner__button');
+
+    title1.innerText = 'Parabéns!!';
+    title2.innerText = 'Você ganhou!';
+    let winnerDifficulty = `<span class="winner__difficulty">${whichDifficulty}</span>`;
+    let winnerMinSteps = `<span class="winner__steps-min">${stepsMin}</span>`;
+    let winnerPlayerSteps = `<span class="winner__steps-player" id="winnerStepsPlayer">${playerSteps}</span>`;
+    textWin.innerHTML = `Para a dificuldade ${winnerDifficulty}, você precisa de no mínimo ${winnerMinSteps} movimentos para vencer, você conseguiu em ${winnerPlayerSteps} movimentos, parabéns!`;
+    winnerButton.innerText = 'Jogar novamente'
+
+    winnerButton.addEventListener('click', playAgain);
+
+    messageWin.appendChild(title1);
+    messageWin.appendChild(title2);
+    messageWin.appendChild(textWin);
+    messageWin.appendChild(winnerButton);
+    return messageWin;
+}
+
+function getPlayerSteps() {
+    const playerSteps = document.querySelector('#stepCountValue');
+    return playerSteps.innerText;
+}
+
+function getDifficultyAndMinSteps() {
+    const difficulty = document.querySelector('#difficultySelector');
+    let difficultyText;
+    let diskQty = difficulty.value;
+    if (diskQty == 3) {
+        difficultyText = 'fácil';
+    } else if (diskQty == 4) {
+        difficultyText = 'médio';
+    } else {
+        difficultyText = 'difícil';
+    }
+    let minSteps = (2 ** diskQty) - 1;
+    return [difficultyText, minSteps];
+}
+
+function blurMainContent() {
+    const mainContainer = document.querySelector('#mainContainer');
+    mainContainer.classList.add('main-container--blurred');
+}
+
+function enableModal() {
+    const mainModal = document.querySelector('#mainModal');
+    mainModal.classList.add('main-modal--enabled');
+}
+
+function displayWinnerMessage() {
+
+    let whichDifficulty = getDifficultyAndMinSteps()[0];
+    let minSteps = getDifficultyAndMinSteps()[1];
+    let playerSteps = getPlayerSteps();
+
+    let message = createVictoryMessage(whichDifficulty, minSteps, playerSteps);
+    document.body.appendChild(message);
+    blurMainContent();
+    enableModal();
+}
+
+function hideWinnerMessage() {
+    const mainModal = document.querySelector('#mainModal');
+    const mainContainer = document.querySelector('#mainContainer');
+    const messageWinner = document.querySelector('#messageWinner');
+    mainContainer.classList.remove('main-container--blurred');
+    mainModal.classList.remove('main-modal--enabled');
+    messageWinner.parentElement.removeChild(messageWinner);
+}
+
+function playAgain() {
+    hideWinnerMessage()
+    reset();
+}
+
+//Função mensagem de vitória
+
+gameSpace.addEventListener("click", victory)
+const message = document.createElement("div")
+
+function victory() {
+    let pile2Count = pile2.childElementCount
+    let pile3Count = pile3.childElementCount
+    if (pile2Count == diskQuantity || pile3Count == diskQuantity) {
+        displayWinnerMessage()
+    }
 }
